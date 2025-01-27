2322640a69e7462b0818d7197be9da8ed8f0e852
jems41 <combjam41@gmail.com>
2025-01-26 21:24:44 -0500

    version 2

--- class-work/week-1/babbling-brooks.py
+++ class-work/week-1/babbling-brooks.py
@@ -1,9 +1,36 @@
-streamAmount = int(input())
-flows = []
+specs = []
 
-for i in range(streamAmount):
-    flow = int(input())
-    flows.append(flow)
+while True:
+    detail = input("Input each details of a stream: ")
+    if detail == '77':
+        specs.append(int(detail))
+        break
+    else:
+        specs.append(int(detail))
 
-for i in range(len(flows)):
-    print("aw hell nah bruh chat help me")
+streams = specs[0]
+
+def babbling_brooks(input, streamAmount):
+    lst = []
+    for i in range(streamAmount):
+        lst.append(input[i+1])
+    for i in range(len(input)):
+        if input[i] == 99:
+            stream = input[i+1]-1
+            ratio = input[i+2]
+            fork1 = lst[stream] * ratio/100
+            fork2 = lst[stream] - fork1
+            lst.pop(stream)
+            lst[stream:stream] = [fork1, fork2]
+        if input[i] == 88:
+            stream_merge = input[i+1]-1
+            merge = lst[stream_merge] + lst[stream_merge+1]
+            lst = lst[:stream_merge] + lst[stream_merge+2:]
+            lst.insert(stream_merge, merge)
+        if input[i] == 77:
+            rounded_list = []
+            for x in lst:
+                rounded_list.append(round(x))
+            return rounded_list
+
+print(babbling_brooks(specs, streams))
