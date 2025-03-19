b50ae40f459bd8468e0c366e3a2115ff36ddc33d
James Combista <combjam41@gmail.com>
2025-03-18 23:27:36 -0400

    1st coding project progress 2

--- coding-projects/music-player/main-program.py
+++ coding-projects/music-player/main-program.py
@@ -1,6 +1,8 @@
+from tkinter import filedialog
 from tkinter import *
 import pygame
 import os
+import random
 
 # create the gui + name + size
 root = Tk()
@@ -9,23 +11,85 @@ root.geometry("640x480")
 
 # import the pygame sound system
 pygame.mixer.init()
-sound = pygame.mixer.Sound('example.wav')
-sound.play() 
 
 # add a menubar to the root window
 menubar = Menu(root)
 root.config(menu=menubar)
 
+songs = []
+current_song = ""
+paused = False
+
+def load_song():
+    global current_song
+    root.directory = filedialog.askdirectory()
+
+    for song in os.listdir(root.directory):
+        name, ext = os.path.splitext(song)
+        if ext == ".wav" or ext == ".mp3":
+            songs.append(song)
+
+    for song in songs:
+        songlist.insert("end", song)
+    
+    if songs:  # Ensure the songs list is not empty
+        songlist.selection_set(0) # selecting first song at the top of the song list
+        current_song = songs[songlist.curselection()[0]] # set the current song to the song that selected in the song list
+        pygame.mixer.music.load(os.path.join(root.directory, current_song)) # constructs the full file path
+        pygame.mixer.music.play()
+
+def pause_song():
+    global paused
+
+    if not paused:
+        pygame.mixer.music.pause()
+        paused = True
+        change_icon(True)
+    else:
+        pygame.mixer.music.unpause()
+        paused = False
+        change_icon(False)
+
+def next_song():
+    global current_song, paused
+    change_icon(False)
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) + 1)
+        current_song = songs[songlist.curselection()[0]]
+    except:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(0)
+        current_song = songs[songlist.curselection()[0]]
+
+    pygame.mixer.music.load(os.path.join(root.directory, current_song))
+    pygame.mixer.music.play()
+    paused = False
+
+def prev_song():
+    global current_song, paused
+    change_icon(False)
+    try:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(songs.index(current_song) - 1)
+        current_song = songs[songlist.curselection()[0]]
+    except:
+        songlist.selection_clear(0, END)
+        songlist.selection_set(len(songs) - 1)
+        current_song = songs[songlist.curselection()[0]]
+
+    pygame.mixer.music.load(os.path.join(root.directory, current_song))
+    pygame.mixer.music.play()
+
 organise_menu = Menu(menubar, tearoff=False) # creating an organise menu
-organise_menu.add_command(label='Select Folder') # add a command to the menu
-menubar.add_cascade(label='Organise', menu=organise_menu) # display it
+organise_menu.add_command(label='Select Folder', command=load_song) # add a command to the menu
+menubar.add_cascade(label='Import', menu=organise_menu) # display it
 
 # adding a songlist
 songlist = Listbox(root, bg='#18363E', fg= 'white', width=102, height=25)
 songlist.pack()
 
 # importing all the images
-play_btn_image = PhotoImage(file='play-button3.png')
 pause_btn_image = PhotoImage(file='pause-button1.png')
 next_btn_image = PhotoImage(file='next.png')
 prev_btn_image = PhotoImage(file='previous.png')
@@ -34,16 +98,33 @@ prev_btn_image = PhotoImage(file='previous.png')
 control_panel = Frame(root)
 control_panel.pack()
 
+# getting value of volume
+def change_volume():
+    volume = scale.get()
+    pygame.mixer.music.set_volume(volume/10)
+
 # creating a button for the image
-play_btn = Button(control_panel, image=play_btn_image, borderwidth=0)
-pause_btn = Button(control_panel, image=pause_btn_image, borderwidth=0)
-next_btn = Button(control_panel, image=next_btn_image, borderwidth=0)
-prev_btn = Button(control_panel, image=prev_btn_image, borderwidth=0)
+pause_btn = Button(control_panel, image=pause_btn_image, borderwidth=0, command=pause_song)
+next_btn = Button(control_panel, image=next_btn_image, borderwidth=0, command=next_song)
+prev_btn = Button(control_panel, image=prev_btn_image, borderwidth=0, command=prev_song)
+scale = Scale(control_panel, from_=0, to=10, orient="horizontal", label='Volume')
+volume_btn = Button(control_panel, text='Update Volume', command=change_volume)
 
 # adding it to the root
-play_btn.grid(row=0, column=1, padx=7, pady=10)
 pause_btn.grid(row=0, column=2, padx=7, pady=10)
 next_btn.grid(row=0, column=3, padx=7, pady=10)
 prev_btn.grid(row=0, column=0, padx=7, pady=10)
+scale.grid(row=0, column=4, padx=7, pady=10, in_=control_panel)
+volume_btn.grid(row=0, column=5, padx=7, pady=10)
+
+# change image of play/resume
+def change_icon(condition):
+    global pause_btn_image
+    if condition:
+        pause_btn_image = PhotoImage(file='play-button3.png')
+    else:
+        pause_btn_image = PhotoImage(file='pause-button1.png')
+    pause_btn.config(image=pause_btn_image)
+    pause_btn.image = pause_btn_image
 
 root.mainloop() # main code to display a screen
